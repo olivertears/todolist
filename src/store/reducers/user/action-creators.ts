afcc7701f42517b87d0d39abdd244ba12e2c8611
Oliver Tears <olivertears@gmail.com>
2022-07-25 22:13:04 +0300

    sign in with google on smartphone2

--- src/store/reducers/user/action-creators.ts
+++ src/store/reducers/user/action-creators.ts
@@ -37,9 +37,11 @@ export const signInWithGoogle = (auth: Auth) => async (dispatch: AppDispatch) =>
   try {
     dispatch(setLoader(true));
     const provider = new GoogleAuthProvider();
-    window.matchMedia('(max-width: 768px)').matches
-      ? await signInWithRedirect(auth, provider)
-      : await signInWithPopup(auth, provider);
+    if (window.innerWidth < 768) {
+      await signInWithRedirect(auth, provider);
+    } else {
+      await signInWithPopup(auth, provider);
+    }
   } finally {
     dispatch(setLoader(false));
   }
