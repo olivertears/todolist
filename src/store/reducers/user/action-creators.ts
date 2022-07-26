f7ff0754bc238040a28873150d3fb7cb9304938c
Oliver Tears <olivertears@gmail.com>
2022-07-26 09:30:51 +0300

    looks like done

--- src/store/reducers/user/action-creators.ts
+++ src/store/reducers/user/action-creators.ts
@@ -37,11 +37,7 @@ export const signInWithGoogle = (auth: Auth) => async (dispatch: AppDispatch) =>
   try {
     dispatch(setLoader(true));
     const provider = new GoogleAuthProvider();
-    if (window.matchMedia('(max-width: 768px)').matches) {
-      await signInWithRedirect(auth, provider);
-    } else {
-      await signInWithPopup(auth, provider);
-    }
+    window.innerWidth < 768 ? await signInWithRedirect(auth, provider) : await signInWithPopup(auth, provider);
   } finally {
     dispatch(setLoader(false));
   }
